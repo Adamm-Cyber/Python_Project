93d5f547f71fd4684fdb5d63df6ef44984ac48d2
adam <adammustafa1919@gmail.com>
2026-02-26 13:53:52 +0200

    A new title has been added, which is variables

--- python_project.py
+++ python_project.py
@@ -1,7 +1,18 @@
 print ("Hello python")
 print("Hallo, ich bin Adam")
-
+#+++++++++++++++++++++++++++++++++++++++++++++
 print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
+#+++++++++++++++++++++++++++++++++++++++++++++
+name = "Adam"
+age = "23"
+print("there was a boy called " + name)
+print("he was " + age + " years old")
+print("he liked his name " + name)
+name = "Hammam"
+age = "21"
+print("there was another boy called " + name)
+print("he was " + age + " years old")
+
