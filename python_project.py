a4c1aeca799cabe41a1eeb31c3ca1f85d90f7c31
adam <adammustafa1919@gmail.com>
2026-02-26 14:22:03 +0200

    A new title has been added, which is: Data Type: String

--- python_project.py
+++ python_project.py
@@ -15,4 +15,14 @@ name = "Hammam"
 age = "21"
 print("there was another boy called " + name)
 print("he was " + age + " years old")
-
+#+++++++++++++++++++++++++++++++++++++++++++++
+print("code\nzilla")
+text = "adam"
+print(text.lower())
+print(text.upper())
+print(text.upper().isupper())
+print(len(text))
+print(text[2])
+print(text.index("a"))
+text = "Codezilla"
+print(text.replace("Code" , "Mode"))
