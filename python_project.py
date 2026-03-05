f955fc7cfac89f931ed40ced0284f2d1fc57e8e3
adam <adammustafa1919@gmail.com>
2026-03-05 13:00:31 +0200

    A new title has been added, which is: Dictionary

--- python_project.py
+++ python_project.py
@@ -22,17 +22,29 @@
 # print(result)
 
 ######OR####
-x = float(input("Enter your first number: "))
-operator = input("Enter the operator: ")
-y = float(input("Enter your second number: "))
+# x = float(input("Enter your first number: "))
+# operator = input("Enter the operator: ")
+# y = float(input("Enter your second number: "))
 
-if operator == "+":
-   print(x+y) 
-elif operator == "-":
-    print(x-y)  
-elif operator == "*":
-    print(x*y)        
-elif operator == "/":
-    print(x/y)
-else: 
-    print("Error, please try again!")
+# if operator == "+":
+#    print(x+y) 
+# elif operator == "-":
+#     print(x-y)  
+# elif operator == "*":
+#     print(x*y)        
+# elif operator == "/":
+#     print(x/y)
+# else: 
+#     print("Error, please try again!")
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+convert_month = {
+    "jan" : "january",
+    "feb" : "febraury",
+    "mar" : "march",
+    1: "septemper"
+}
+
+print(convert_month["jan"])
+#or 
+print(convert_month.get("feb" , "the value does not exist"))
+print(convert_month.get(1 , "the value does not exist"))
