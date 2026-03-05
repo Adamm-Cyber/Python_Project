eb441735f60b8241d316fe6600d8996ccc9f5745
adam <adammustafa1919@gmail.com>
2026-03-05 13:30:25 +0200

    A new title has been added, which is:while loop

--- python_project.py
+++ python_project.py
@@ -37,14 +37,39 @@
 # else: 
 #     print("Error, please try again!")
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++
-convert_month = {
-    "jan" : "january",
-    "feb" : "febraury",
-    "mar" : "march",
-    1: "septemper"
-}
+# convert_month = {
+#     ####    key:value 
+#     "jan" : "january",
+#     "feb" : "febraury",
+#     "mar" : "march",
+#     1: "septemper"
+# }
 
-print(convert_month["jan"])
-#or 
-print(convert_month.get("feb" , "the value does not exist"))
-print(convert_month.get(1 , "the value does not exist"))
+# print(convert_month["jan"])
+# #or 
+# print(convert_month.get("feb" , "the value does not exist"))
+# print(convert_month.get(1 , "the value does not exist"))
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# i=1
+# while i <= 10:
+#     print(i)
+#     i+=1
+# print("the loop has ended") 
+#+++++++++++++++++++++++++++++++++++
+# i=1
+# while i <= 10:
+#     i+=1
+#     if i == 6:
+#         continue
+#     print(i)
+
+# print("the loop has ended")  
+#+++++++++++++++++++++++++++++++++++  
+i=1
+while i <= 10:
+    i+=1
+    if i == 6:
+        break
+    print(i)
+
+print("the loop has ended") 
